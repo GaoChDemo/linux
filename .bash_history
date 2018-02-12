cb241e3a12f6db912eeea94b289266a254bbcd91
gaochongmain <gaochong.main@gmail.com>
2018-02-12 10:51:46 +0800

    1.2

--- .bash_history
+++ .bash_history
@@ -73,3 +73,192 @@ vim DouBanRe.py
 python DouBanRe.py
 ls
 rm g.csv 
+ls
+git add .
+ls
+git push origin master
+git status
+git commit
+ls
+git commit -m "1"
+ls
+git push origin master
+ls
+cd ~
+ls
+sudo apt-get install mysql-server
+sudo apt isntall mysql-client
+sudo apt install libmysqlclient-dev
+sudo netstat -tap | grep mysql
+sudo apt-get install mysql-server
+apt-get update
+sudo apt-get install mysql-server
+sudo apt isntall mysql-client
+apt install mysql-client
+sudo apt install libmysqlclient-dev
+sudo netstat -tap | grep mysql
+mysql -u root -p
+mysql -u root -r
+cd /etc/
+ls
+cd init.d
+ls
+cd mysql
+mysqld stop
+cd /etc/
+cd mysql
+vim mysql.conf.d/
+mysql -u root -h localhost -p
+ls
+cd /etc/
+cd mysql
+net start mysql
+ls
+cd 
+ls
+cd ..
+ls
+cd ~
+ls
+dir
+pwd
+ls
+python DoubanAll.py 
+pip install pandas
+python DoubanAll.py 
+ls
+ls -r
+ls -r -l
+vim DoubanAll.py 
+ls -r -l
+python DoubanAll.py 
+ls -r -l
+python DoubanAll.py 
+ls -r -l
+ls
+ps
+ps -U
+ps 
+ls -r -l
+vim DoubanAll.py 
+
+ps
+vim DoubanAll.py 
+python DoubanAll.py 
+ls
+ps
+ls -r -l
+python DoubanAll.py 
+python
+ls
+python DouBanRe.py 
+ls
+rm g.csv 
+vim DouBanRe.py 
+python DouBanRe.py 
+ls -r -f
+ls -r -h
+ls -r
+ls -r -l
+git add .
+git commit
+git push origin master
+git add .
+git status
+git commit -u
+git commit -m
+git commit
+git commit -m
+git status
+git pull origin master
+python DouBanRe.py 
+ls
+ls -r -l
+rm gcsv
+rm g.csv
+ls
+python DouBanRe.py 
+ls
+ls -r -l
+python DouBanRe.py 
+ls
+vim DouBanRe.py 
+ls
+python DouBanRe.py 
+ls
+cd /etc/
+ls
+cd mysql/
+mysql.conf.d/
+vim mysql.conf.d/
+ls
+vim musql.cnf
+ls
+vim mysql.conf.d
+ls
+vim mysql.conf.d
+mysql -u root -r
+mysql -u root -h
+mysql -u root -r
+ls
+cd ~
+ls
+rm rrr.csv 
+python Dou.py
+vim Dou.py
+python Dou.py
+/etc/
+cd /etc/
+ls
+cd default/
+cd locale
+ls
+vim locale
+reboot
+vim Dou.py
+python Dou.py
+vim Dou.py
+python Dou.py
+vim Dou.py
+rm Dou.py
+vim Dou.py
+python Dou.py
+vim Dou.py
+rm Dou.py
+vim Dou.py
+python Dou.py
+vim Dou.py
+ls
+python Dou.py
+vim /etc/default/locale 
+reboot
+vim Dou.py
+rm Dou.py
+vim Dou.py
+
+vim Dou.py
+rm Dou.py
+vim Dou.py
+python Dou.py
+ls -r
+ls -l
+python Dou.py
+rm Dou.py
+vim Dou.py
+rm Dou.py
+vim Dou.py
+pythhon Dou.py
+python Dou.py
+vim Dou.py
+rm Dou.py
+vim Dou.py
+python Dou.py
+vim Dou.py
+python
+rm Dou.py
+vim Dou.py
+python Dou.py
+python Dou.py
+vim Dou.py
+python Dou.py
+vim Dou.py
--- Dou.py
+++ Dou.py
@@ -6,6 +6,8 @@ import pandas as pd
 import time
 import random
 import string
+import json
+
 
 USER_AGENTS = [
     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
@@ -52,54 +54,11 @@ for i in range(0,250,25):
 		print pq(movie).find('.rating_num').html(),
 		#print pq(movie).find('.bd').html()'''
 
-class Douban_url:
-	"""获取所有 """
-	def __init__(self):
-		self.url_list = []
-		self.headers = {}
-	def get_headers(self):
-		self.headers = {
-	        "User-Agent": random.choice(USER_AGENTS),
-	        "Host": "movie.douban.com",
-	        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-	        "Accept-Encoding": "gzip, deflate, sdch, br",
-	        "Accept-Language": "zh-CN, zh; q=0.8, en; q=0.6",
-	        "Cookie": "bid=%s" % "".join(random.sample(string.ascii_letters + string.digits, 11))
-    	}
-
-	def get_url(self,page,url):
-		#print pq(page).find('.next').attr('href')
-		return url + pq(page).find('.next').find('a').attr('href')
-	def downloader(self,url):
-		r = requests.get(url,headers=self.headers)
-		return r.text
-	def html_paser(self,page):
-		for item in pq(page).find('.item'):
-			url = pq(item).find('.pic').find('a').attr('href') + 'comments?status=P'
-			#print url
-			self.url_list.append(url)
-	def start(self):
-		url_start = 'https://movie.douban.com/top250'
-		url_root = url_start.split('?')[0]
-		#print url_root
-		url = url_start
-		while(True):
-			#print url
-			try:
-				self.get_headers()
-				page = self.downloader(url)
-				self.html_paser(page)
-				url = self.get_url(page,url_root)
-			except Exception as e:
-				#print e
-				break
-	def get_list(self):
-		return self.url_list
-
 class Douban:
 	"""demo """
-	def __init__(self,url_start):
-		self.url_start = url_start
+	def __init__(self):
+		self.url_list = []
+		self.url_start = ''
 		self.data = []
 		self.headers = {}
 	def get_headers(self):
@@ -111,18 +70,42 @@ class Douban:
 	        "Accept-Language": "zh-CN, zh; q=0.8, en; q=0.6",
 	        "Cookie": "bid=%s" % "".join(random.sample(string.ascii_letters + string.digits, 11))
     	}
-	def get_url(self,page,url):
+
+	def downloader_movie_list(self,url,number):
+		r = requests.get(url,headers=self.headers,allow_redirects=False)
+		print 'download'
+		while r.status_code != 200:
+			print 'download failed, wait for 10 minutes %d' % number
+			time.sleep(600)
+			r = requests.get(url,headers=self.headers,allow_redirects=False)
+		return json.loads(r.text)
+
+	def get_movie(self):
+		url_start = 'https://movie.douban.com/j/new_search_subjects?sort=T&range=0,5&tags=&start=%d'
+		for i in range(0,200,20):
+			url = url_start % i
+			self.get_headers()
+			urllist = self.downloader_movie_list(url,i)['data']
+			self.url_list += urllist
+
+
+	def get_url(self,page,url,num):
 		#print pq(page).find('.next').attr('href')
-		return url + pq(page).find('.next').attr('href')
-	def downloader(self,url):
-		r = requests.get(url,headers=self.headers)
+		url = url + '?start=%d&limit=20&sort=new_score&status=P&percent_type=' % num
+		return url
+	def downloader(self,url,movie):
+		r = requests.get(url,headers=self.headers,allow_redirects=False)
+		while r.status_code != 200:
+			print 'download failed, wait for 10 minutes'
+			time.sleep(600)
+			r = requests.get(url,headers=self.headers,allow_redirects=False)
 		return r.text
 	def html_paser(self,page,movie):
 		for item in pq(page).find('.comment-item'):
 			user = pq(item).find('.comment-info').find('a').html()
 			rating = pq(item).find('.rating').attr('title')
 			time = pq(item).find('.comment-time').attr('title')
-			text = pq(item).find('.comment').find('p').html()
+			text = pq(item).find('.comment').find('p').html().split('<a class="source-icon"')[0].strip().replace('"', '').replace('\n', '')
 			self.data.append({'movie':movie,'user':user,'rating':rating,'time':time,'text':text})
 	def output(self):
 		for item in self.data:
@@ -130,21 +113,48 @@ class Douban:
 			print item['title'],"  ",item['rating']
 		#dataframe.to_csv("test.csv",sep=',')
 	def writefile(self):
-		global num
 		df = pd.DataFrame(self.data,columns=['movie','user','rating','time','text'])
-		df.to_csv('w.csv', sep=',', header=False, index=False,encoding='utf-8',mode='a')
-	def start(self,num=250):
+		df.to_csv('rrr.csv', sep=',', header=False, index=False,encoding='utf-8',mode='a')
+		self.data = []
+	def start(self):
+		df = pd.DataFrame(columns=['movie','user','rating','time','text'])
+		df.to_csv('rrr.csv', sep=',', header=True, index=False,encoding='utf-8',mode='w')
+		self.get_headers()
+		self.get_movie()
+		while(len(self.url_list) > 0):
+			time.sleep(10)
+			smovie = self.url_list.pop()
+
+			url = smovie['url']+'comments?status=P'
+			url_root = url.split('?')[0]
+			#print url_root	
+			print url
+			for i in range(0,200,20):
+				
+				time.sleep(2)
+				try:
+					self.get_headers()
+					movie = smovie['title']
+					page = self.downloader(url,movie)
+					#print movie
+					self.html_paser(page,movie)
+					url = self.get_url(page,url_root,i)
+				except Exception as e:
+					#print e
+					print e
+					break
+			
+			self.writefile()
+			'''
 		url_root = self.url_start.split('?')[0]
 		#print url_root
 		url = self.url_start
 		while(True):
-			time.sleep(2)
+			time.sleep(1)
 			print url
 			try:
 				self.get_headers()
 				page = self.downloader(url)
-				movie = pq(page).find('#content').find('h1').html()
-				movie = movie.split(' ')[0]
 				#print movie
 				self.html_paser(page,movie)
 				url = self.get_url(page,url_root)
@@ -152,7 +162,7 @@ class Douban:
 				#print e
 				break
 			
-		self.writefile()
+		self.writefile()'''
 	
 '''	
 		for url in self.get_url(num):
@@ -163,16 +173,23 @@ class Douban:
 
 #d = Douban()
 #d.start()
-u = Douban_url()
-
+u = Douban()
 u.start()
+'''
+for lis in u.get_movie():
+	while(len(lis['data']) > 0):
+	    movie = lis['data'].pop()
+	    print movie['title'],movie['url']
+
+
 url_list = u.get_list()
 df = pd.DataFrame(columns=['movie','user','rating','time','text'])
-df.to_csv('w.csv', sep=',', header=True, index=False,encoding='utf-8',mode='a')
-while(len(url_list)>0):
+df.to_csv('rrr.csv', sep=',', header=True, index=False,encoding='utf-8',mode='a')
+for i in range(2):
+#while(len(url_list)>0):
 	d = Douban(url_list.pop())
 	d.start()
-	time.sleep(120)
+	time.sleep(5)
 
-#print cookies
+#print cookies'''
 
